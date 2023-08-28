421c31542f6833ab1edd3c4f34a5139fb2e92201
Bocianski <szyper212pl@gmail.com>
2023-08-28 11:19:47 +0200

    tests update

---
+++ kataphron/combat.py
@@ -0,0 +1,3 @@
+"""
+2 models stand next to themselfs and take turns making all through all battle phases. Distnace between them will be set in constructor. First turn takes
+"""
--- tests/test_info.py
+++ tests/test_info.py
@@ -1,20 +1,31 @@
 import unittest
 
 from kataphron.info import Destroyer, Breacher
-from kataphron.weapons import ArcClaw, HeavyArcRifle, PlasmaCulverin, CognisFlamer
+from kataphron.weapons import ArcClaw, HeavyArcRifle, PlasmaCulverin, CognisFlamer, Weapon, TorsionCannon
+
+
+class WeaponTests(unittest.TestCase):
+    def test_constructor(self):
+        self.assertTrue(Weapon is not None)
+
+    def test_different_types(self):
+        self.assertFalse(self, CognisFlamer.type == ArcClaw.type)
+
+    def test_special_rules(self):
+        self.assertTrue(self, TorsionCannon.abilities == "")
 
 
 class KataphronTests(unittest.TestCase):
-    def test_constructor(self):
+    def test_kataphton_constructor(self):
         self.assertTrue(Breacher(HeavyArcRifle, ArcClaw) is not None)
 
 
 class DestroyerTests(unittest.TestCase):
-    def test_destroyer_constuctor(self):
+    def test_destroyer_constructor(self):
         self.assertTrue(Destroyer(PlasmaCulverin, CognisFlamer) is not None)
 
     def test_destroyer_stats_size(self):
         self.assertTrue(len(Destroyer(PlasmaCulverin, CognisFlamer).stats) == 9)
 
     def test_destroyer_one_stat(self):
-        Destroyer
+        self.assertEquals(Destroyer(PlasmaCulverin, CognisFlamer).stats.get(self, "W"), Breacher(HeavyArcRifle, ArcClaw).stats.get(self, "W"))
